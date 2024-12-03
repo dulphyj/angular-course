b46864cce06fa77f114363a56bf1915a9f9b87d1
Gonzalo Encinas <dulphyj@gmail.com>
2024-12-03 19:30:13 -0400

    environment config

--- client-control/src/main.ts
+++ client-control/src/main.ts
@@ -1,6 +1,24 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { environment } from './environments/environment.development';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 
-bootstrapApplication(AppComponent, appConfig)
+
+bootstrapApplication(AppComponent,
+  {
+    ...appConfig,
+    providers: [
+      provideHttpClient(),
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+      provideFirestore(() => getFirestore()),
+      provideStorage(() => getStorage()),
+      provideAuth(() => getAuth()),
+      ...appConfig.providers
+    ]
+  })
   .catch((err) => console.error(err));
