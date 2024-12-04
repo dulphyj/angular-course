adb608e5d2c6cb684bbe6e1b0ed61066132db8c7
Gonzalo Encinas <dulphyj@gmail.com>
2024-12-04 15:55:39 -0400

    added modal form for add client

--- client-control/src/app/components/clients/clients.component.ts
+++ client-control/src/app/components/clients/clients.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Client } from '../../model/client.model';
 import { ClientService } from '../../services/client.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-clients',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './clients.component.html',
   styleUrl: './clients.component.css'
 })
@@ -20,6 +21,10 @@ export class ClientsComponent {
     this.clientService.getClients().subscribe(clients => {
       this.clients = clients
     })
+  }
+
+  getTotalBalance(): number {
+    return this.clients?.reduce((total, client) => total + (client.balance ?? 0), 0) ?? 0
   }
 
 }
